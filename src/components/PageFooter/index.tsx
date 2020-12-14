32c0f599cd22d096970ed21e0ae45aef44642209
anshij <ansj@yunheit.com>
2020-12-14 13:41:45 +0800

    feat: 完成底部导航栏的布局

--- src/components/PageFooter/index.tsx
+++ src/components/PageFooter/index.tsx
@@ -1,6 +1,40 @@
 import React, { useState } from 'react';
 import { TabBar } from 'antd-mobile';
 import styles from './index.module.less';
+import {
+  BsHouseDoor,
+  BsPersonDash,
+  BsCalendar,
+  BsPersonBoundingBox,
+} from 'react-icons/bs';
+
+interface ListItem {
+  text: string;
+  icon: JSX.Element;
+  key: string;
+}
+const lists: ListItem[] = [
+  {
+    text: '首页',
+    icon: <BsHouseDoor />,
+    key: 'home',
+  },
+  {
+    text: '我要出租',
+    icon: <BsPersonDash />,
+    key: 'torent',
+  },
+  {
+    text: '看房日程',
+    icon: <BsCalendar />,
+    key: 'day',
+  },
+  {
+    text: '个人中心',
+    icon: <BsPersonBoundingBox />,
+    key: 'me',
+  },
+];
 
 const PageFooter: React.FC = () => {
   const [selectedTab, setSelectedTab] = useState<string>('life');
@@ -8,118 +42,23 @@ const PageFooter: React.FC = () => {
     <footer className={styles['page-footer--layout']}>
       <TabBar
         unselectedTintColor="#949494"
-        tintColor="#33A3F4"
-        barTintColor="white"
+        tintColor="#f58611"
+        barTintColor="#fff"
         hidden={false}
         tabBarPosition="top"
       >
-        <TabBar.Item
-          title="Life"
-          key="Life"
-          icon={
-            <div
-              style={{
-                width: '22px',
-                height: '22px',
-                background:
-                  'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat',
-              }}
-            />
-          }
-          selectedIcon={
-            <div
-              style={{
-                width: '22px',
-                height: '22px',
-                background:
-                  'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat',
-              }}
-            />
-          }
-          selected={selectedTab === 'blueTab'}
-          badge={1}
-          onPress={() => {
-            setSelectedTab('blueTab');
-          }}
-          data-seed="logId"
-        />
-        <TabBar.Item
-          icon={
-            <div
-              style={{
-                width: '22px',
-                height: '22px',
-                background:
-                  'url(https://gw.alipayobjects.com/zos/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat',
-              }}
-            />
-          }
-          selectedIcon={
-            <div
-              style={{
-                width: '22px',
-                height: '22px',
-                background:
-                  'url(https://gw.alipayobjects.com/zos/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat',
-              }}
-            />
-          }
-          title="Koubei"
-          key="Koubei"
-          badge={'new'}
-          selected={selectedTab === 'redTab'}
-          onPress={() => {
-            setSelectedTab('redTab');
-          }}
-          data-seed="logId1"
-        />
-
-        <TabBar.Item
-          icon={
-            <div
-              style={{
-                width: '22px',
-                height: '22px',
-                background:
-                  'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  21px 21px no-repeat',
-              }}
-            />
-          }
-          selectedIcon={
-            <div
-              style={{
-                width: '22px',
-                height: '22px',
-                background:
-                  'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  21px 21px no-repeat',
-              }}
-            />
-          }
-          title="Friend"
-          key="Friend"
-          dot
-          selected={selectedTab === 'greenTab'}
-          onPress={() => {
-            setSelectedTab('greenTab');
-          }}
-        />
-
-        <TabBar.Item
-          icon={{
-            uri:
-              'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg',
-          }}
-          selectedIcon={{
-            uri:
-              'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg',
-          }}
-          title="My"
-          key="my"
-          selected={selectedTab === 'yellowTab'}
-          onPress={() => {
-            setSelectedTab('yellowTab');
-          }}
-        />
+        {lists.map((item) => (
+          <TabBar.Item
+            title={item.text}
+            key={item.text}
+            icon={item.icon}
+            selectedIcon={item.icon}
+            selected={selectedTab === item.key}
+            onPress={() => {
+              setSelectedTab(item.key);
+            }}
+          />
+        ))}
       </TabBar>
     </footer>
   );
